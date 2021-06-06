881b3284d7d91c32f009cd4df7a7db7e2141e184
Mustafa Tuncay <mustafa.tuncay@sozcu.com.tr>
2021-06-06 14:33:53 +0300

    port fixed

--- src/index.js
+++ src/index.js
@@ -2,36 +2,36 @@
 import cors from "cors";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import DotEnv from 'dotenv';
-import todoRoutes from 'routes/Todo'
+import DotEnv from "dotenv";
+import todoRoutes from "routes/Todo";
 import express from "express";
 
 DotEnv.config();
 // we are using port 8000
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 const app = express();
 
 // DB connection
 mongoose
-  .connect(process.env.CONNECTION_STRING, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then(() => {
-    console.log("CONNECTED TO DATABASE");
-  });
+    .connect(process.env.CONNECTION_STRING, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .then(() => {
+        console.log("CONNECTED TO DATABASE");
+    });
 
 app.use(cors());
 // middleware to convert our request data into JSON format
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // include the todoRoutes
 app.use("/api", todoRoutes);
 
 // start the server in the port 8000
 app.listen(port, () => {
-  console.log(`Listening to http://localhost:${port}`);
+    console.log(`Listening to http://localhost:${port}`);
 });
